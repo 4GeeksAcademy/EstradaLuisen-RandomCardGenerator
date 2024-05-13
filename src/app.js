30c41e29000a174f129ab164d0d2c5a101605d6b
EstradaLuisen <143045575+EstradaLuisen@users.noreply.github.com>
2024-05-13 22:43:57 +0000

    ADD the random number and symbol funcionalities

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const NUMBERS = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const SYMBOLS = ["♦", "♥", "♠", "♣"];
+function arrayLocation(numberLength) {
+  return Math.floor(Math.random() * numberLength);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Get a random number and also a random symbol
+  let number = NUMBERS[arrayLocation(NUMBERS.length)];
+  let symbol = SYMBOLS[arrayLocation(SYMBOLS.length)];
+  console.log(NUMBERS[number]);
+  console.log(SYMBOLS[symbol]);
+  //Put the random number in the DIV
+  let numberDIV = document.querySelector("#divNumber");
+  numberDIV.innerHTML = `<p class="cardNum">${number}</p>`;
+  //Put the symbol and the color needed
+  let symbolDIVs = document.querySelectorAll(".symbClass");
+  symbolDIVs.forEach(function(symbolDIV) {
+    symbol == "♦" || symbol == "♥"
+      ? (symbolDIV.innerHTML = `<p class="cardSymb text-danger">${symbol}</p>`)
+      : (symbolDIV.innerHTML = `<p class="cardSymb">${symbol}</p>`);
+  });
 };
